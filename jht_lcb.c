28db69b7fdfcd792bb629c52345fe7632ffa2a68
peterwu <peterwu@johnsonfitness.com>
2024-08-01 09:28:45 +0800

    [LCB] add LCB_TYPE for bike and treadmill

--- jht_lcb.c
+++ jht_lcb.c
@@ -16,8 +16,14 @@
 
 #define MAX_REPLY_SIZE 128
 
+typedef enum {
+	BIKE = 0,
+	TREADMILL = 1,
+} JHT_LCB_TYPE_t;
+
 static int fd;
 static int debug = 0;
+static JHT_LCB_TYPE_t lcb_type = BIKE;
 volatile sig_atomic_t run_flag = 1;
 
 static void exit_handler(int sig) {	// can be called asynchronously
@@ -60,6 +66,27 @@ unsigned char Get_CRC8(char *Ptr, unsigned char DtaLen)
 	return  CRC_Data;
 }
 
+int setRTS(int fd, int level)
+{
+	int status;
+
+	if (ioctl(fd, TIOCMGET, &status) == -1) {
+		perror("setRTS(): TIOCMGET");
+		return -1;
+	}
+
+	if (level == 0)
+		status |= TIOCM_RTS;	// RTS low
+	else
+		status &= ~TIOCM_RTS;	// RTS high
+
+	if (ioctl(fd, TIOCMSET, &status) == -1) {
+		perror("setRTS(): TIOCMSET");
+		return -1;
+	}
+	return 0;
+}
+
 int rs485_enable(const int fd, bool enable)
 {
 	struct serial_rs485 rs485conf;
@@ -78,8 +105,6 @@ int rs485_enable(const int fd, bool enable)
 	} else {		// Disable rs485 mode
 		rs485conf.flags &= ~(SER_RS485_ENABLED);
 	}
-
-	rs485conf.delay_rts_before_send = 0x00000004;
 
 	/* Set configure to device */
 	res = ioctl(fd, TIOCSRS485, &rs485conf);
@@ -168,7 +193,7 @@ int uart_gets(char *msg, int len)
 	FD_ZERO(&fds);
 	FD_SET(fd, &fds);
 	timeout.tv_sec = 0;
-	timeout.tv_usec = 200 * 1000;	// 200ms
+	timeout.tv_usec = 100 * 1000;	// 100ms
 	ret = select(FD_SETSIZE, &fds, NULL, NULL, &timeout);
 	if (ret == 1) {
 		n = read(fd, msg, len);
@@ -185,51 +210,39 @@ int uart_gets(char *msg, int len)
 }
 
 void jht_get_version(void) {
-#define CMD_SIZE 5
-	char data[CMD_SIZE];
+	char data[ ] = {0x00, 0xFF, 0x73, 0x00, 0x00};
+	int cmd_size = sizeof(data);
 
-	data[0] = 0x00;
-	data[1] = 0xFF;
-	data[2] = 0x73;
-	data[3] = 0x00;
-	data[4] = Get_CRC8(data, CMD_SIZE-1);
-	uart_puts(data, CMD_SIZE);
+	data[cmd_size-1] = Get_CRC8(data, cmd_size-1);
+	uart_puts(data, cmd_size);
 }
 
 void jht_get_status(void) {
-#define CMD_SIZE 5
-	char data[CMD_SIZE];
+	char data[ ] = {0x00, 0xFF, 0x71, 0x00, 0x00};
+	int cmd_size = sizeof(data);
 
-	data[0] = 0x00;
-	data[1] = 0xFF;
-	data[2] = 0x71;
-	data[3] = 0x00;
-	data[4] = Get_CRC8(data, CMD_SIZE-1);
-	uart_puts(data, CMD_SIZE);
+	data[cmd_size-1] = Get_CRC8(data, cmd_size-1);
+	uart_puts(data, cmd_size);
 }
 
 void jht_get_error_code(void) {
-#define CMD_SIZE 5
-	char data[CMD_SIZE];
+	char data[ ] = {0x00, 0xFF, 0x72, 0x00, 0x00};
+	int cmd_size = sizeof(data);
 
-	data[0] = 0x00;
-	data[1] = 0xFF;
-	data[2] = 0x72;
-	data[3] = 0x00;
-	data[4] = Get_CRC8(data, CMD_SIZE-1);
-	uart_puts(data, CMD_SIZE);
+	data[cmd_size-1] = Get_CRC8(data, cmd_size-1);
+	uart_puts(data, cmd_size);
 }
 
 void jht_get_rpm(void) {
-#define CMD_SIZE 5
-	char data[CMD_SIZE];
+	char data[ ] = {0x00, 0xFF, 0x63, 0x00, 0x00};	// bike
+	int cmd_size = sizeof(data);
 
-	data[0] = 0x00;
-	data[1] = 0xFF;
-	data[2] = 0x63;//0xF9;
-	data[3] = 0x00;
-	data[4] = Get_CRC8(data, CMD_SIZE-1);
-	uart_puts(data, CMD_SIZE);
+	if (lcb_type == TREADMILL) {
+		data[2] = 0xF9;		// treadmill
+	}
+
+	data[cmd_size-1] = Get_CRC8(data, cmd_size-1);
+	uart_puts(data, cmd_size);
 }
 
 int main(int argc, char **argv) {
@@ -264,7 +277,6 @@ int main(int argc, char **argv) {
 	len = uart_gets(rx_buf, MAX_REPLY_SIZE);
 	sleep(1);
 
-	// wayne say : if not zero , need get error code once
 	if (rx_buf[3] != 0) {
 		jht_get_error_code();
 		len = uart_gets(rx_buf, MAX_REPLY_SIZE);
@@ -279,6 +291,13 @@ int main(int argc, char **argv) {
 	len = uart_gets(rx_buf, MAX_REPLY_SIZE);
 	printf("version: %x %x\n", rx_buf[5], rx_buf[4]);
 
+	if ((rx_buf[5] == 4) || (rx_buf[5] == 7)) {
+		lcb_type = BIKE;
+	} else {
+		lcb_type = TREADMILL;
+	}
+
+
 	while (run_flag) {
 		jht_get_rpm();
 
@@ -291,9 +310,10 @@ int main(int argc, char **argv) {
 				// checkk CRC
 				if (Get_CRC8(rx_buf, len-1) == rx_buf[len-1]) {
 					// print rpm
-					if (rx_buf[2] == 0x63) {
+					if (rx_buf[2] == 0x63)
 						printf("rpm: %d\n", (rx_buf[4] << 8) | rx_buf[5]);
-					}
+					else if (rx_buf[2] == 0xF9)
+						printf("rpm: %d\n", (rx_buf[4] << 8) | rx_buf[5]);
 				} else {
 					printf("crc8 is dismatch 0x%02X 0x%02X\n", Get_CRC8(rx_buf, len-1), rx_buf[len-1]);
 				}
